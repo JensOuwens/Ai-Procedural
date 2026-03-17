bd60d1d418824ed264e09926bfd81e3b85ee4028
Jens Ouwens <jensouwe@xs4all.nl>
2026-03-17 15:38:12 +0100

    Added: player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
-using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour, IDamageable
 {
@@ -12,11 +13,23 @@ public class PlayerMovement : MonoBehaviour, IDamageable
     [Header("Movement")] 
     [SerializeField]
     private float moveSpeed;
+    private Vector2 input;
 
     private void OnValidate()
     {
         maxHealth = setMaxHealth;
         currentHealth = setMaxHealth;
+        
+    }
+
+    private void Update()
+    {
+            gameObject.transform.position += new Vector3(input.x * moveSpeed, 0 , input.y * moveSpeed);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
     }
     
     
